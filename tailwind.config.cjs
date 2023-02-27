a22497f8011a142c47b3283e8a5f2593a93cdddc
Justinnn <63072677+Jqstln@users.noreply.github.com>
2023-02-27 20:38:29 +0100

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,md,mdx,svelte,ts,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,md,mdx,svelte,ts,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('flowbite/plugin')
+	],
 }
